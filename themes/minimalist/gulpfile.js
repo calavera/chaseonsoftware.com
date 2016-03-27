8eaef6d941eaeb7af7cb973075eadc8b31e756ea
Chase Adams <chasebuildssoftware@gmail.com>
2016-03-26 18:45:27 -0700

    update to working config

--- themes/minimalist/gulpfile.js
+++ themes/minimalist/gulpfile.js
@@ -3,12 +3,12 @@ const sass = require("gulp-sass");
 
 gulp.task("default", ["sass"]);
 
-gulp.task("sass", () => {
+gulp.task("sass", function () {
   return gulp.src("./src/sass/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./static/css"))
 });
 
-gulp.task("watch", () => {
+gulp.task("watch", function () {
   gulp.watch("./src/sass/**/*.scss", ["sass"])
 });
