da5babdaa1c9fc663e215e0c949b659178023dfa
Chase Adams <hey@chaseadams.io>
2018-06-09 22:49:11 -0700

    add actual contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 export default () => (
-  <div>
+  <div className="small-container pad-container">
     <h1>I'd love to talk! Email me at the address below</h1>
-    <p>
-      <a href="mailto:me@example.com">me@example.com</a>
-    </p>
+    <form name="contact" method="POST" netlify>
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </div>
 );
