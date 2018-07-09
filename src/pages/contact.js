668c1fac1722838f791d49f1254d2a2e52583748
Chase Adams <hey@chaseadams.io>
2018-07-09 07:28:58 -0700

    abstract info, form, submit button contact elements into components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,50 @@ function encode(data) {
     .join("&");
 }
 
-class ContactPage extends React.Component {
+const Input = props => (
+  <div className={props.shouldMarkError ? "error input" : "input"}>
+    <label htmlFor="name">name</label>
+    <input
+      type="text"
+      name={props.name}
+      autoComplete={props.name}
+      placeholder={props.placeholder}
+      value={props.name}
+      onBlur={props.onBlur}
+      onChange={props.onChange}
+    />
+    {props.shouldMarkError && <span>props.errorMsg</span>}
+  </div>
+);
+
+const ContactInfo = () => (
+  <div className="c-2-3">
+    <h2>You can find me...</h2>{" "}
+    <ul>
+      <li>
+        talking about software, distributed teams & work on{" "}
+        <a href="https://twitter.com/chaseadamsio">Twitter</a>.
+      </li>{" "}
+      <li>
+        building software on{" "}
+        <a href="https://gitlab.com/chaseadamsio">GitLab</a> and{" "}
+        <a href="https://github.com/chaseadamsio">GitHub</a>.
+      </li>
+      <li>
+        also writing on <a href="https://medium.com/chaseadamsio">Medium</a>.
+      </li>
+      <li>
+        professionally on{" "}
+        <a href="https://www.linkedin.com/in/chaseadams/">LinkedIn</a>.
+      </li>
+    </ul>
+    <p style={{ fontStyle: "italic" }}>
+      Can't find a place that makes sense? Reach out through the form!
+    </p>
+  </div>
+);
+
+class ContactForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -28,7 +71,6 @@ class ContactPage extends React.Component {
       }
     };
   }
-
   handleChange = e => {
     const { name, value } = e.target;
     this.setState(prevState => ({
@@ -83,7 +125,7 @@ class ContactPage extends React.Component {
       );
   };
 
-  render(location) {
+  render() {
     const { input } = this.state;
     const errors = this.validate(
       input.name,
@@ -99,6 +141,121 @@ class ContactPage extends React.Component {
     };
 
     return (
+      <div className="c-1-3">
+        {this.state.errors.server && <p>{this.state.errors.server}</p>}
+        {this.state.status === "success" && <p>Thanks for reaching out!</p>}
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" />
+            </label>
+          </p>
+
+          <div className={shouldMarkError("reason") ? "error input" : "input"}>
+            <select
+              value={input.reason}
+              onBlur={this.handleBlur("reason")}
+              onChange={this.handleChange}
+              name="reason"
+            >
+              <option value="" disabled="" defaultValue="">
+                What's up?
+              </option>
+              {[
+                "I want to learn something.",
+                "I want to recruit you.",
+                "Just saying hi."
+              ].map((option, idx) => {
+                return (
+                  <option key={idx} name="reason" value={option}>
+                    {option}
+                  </option>
+                );
+              })}
+            </select>
+            {shouldMarkError("reason") && (
+              <span className="error">
+                Choosing a reason helps me respond better!
+              </span>
+            )}
+          </div>
+
+          <div className="group pad-bottom-container">
+            <div className={shouldMarkError("name") ? "error input" : "input"}>
+              <label htmlFor="name">name</label>
+              <input
+                type="text"
+                name="name"
+                autoComplete="name"
+                placeholder="Chase Adams"
+                value={input.name}
+                onBlur={this.handleBlur("name")}
+                onChange={this.handleChange}
+              />
+              {shouldMarkError("name") && (
+                <span>Your name helps me know who you are!</span>
+              )}
+            </div>
+
+            <div className={shouldMarkError("email") ? "error input" : "input"}>
+              <label htmlFor="email">email</label>
+              <input
+                type="text"
+                name="email"
+                autoComplete="email"
+                placeholder="hey@chaseadams.io"
+                value={input.email}
+                onBlur={this.handleBlur("email")}
+                onChange={this.handleChange}
+              />
+              {shouldMarkError("email") && (
+                <span>Knowing your email enables me respond!</span>
+              )}
+            </div>
+          </div>
+
+          <div className={shouldMarkError("message") ? "error input" : "input"}>
+            <label>message</label>
+            <textarea
+              name="message"
+              placeholder="Hey, I've got something to say!"
+              value={input.message}
+              onBlur={this.handleBlur("message")}
+              onChange={this.handleChange}
+            />
+            {shouldMarkError("message") && (
+              <span>A message gives me context!</span>
+            )}
+          </div>
+          <SubmitButton isEnabled={isEnabled} />
+        </form>
+      </div>
+    );
+  }
+}
+
+const SubmitButton = props => (
+  <div>
+    <button
+      className={!props.isEnabled ? "error" : ""}
+      disabled={!props.isEnabled}
+      type="submit"
+    >
+      {!props.isEnabled ? "The form is incomplete..." : "Send"}
+    </button>
+  </div>
+);
+
+class ContactPage extends React.Component {
+  render(location) {
+    return (
       <Layout location={location}>
         <Helmet title="Say Hi | Chase Adams" />
         <article style={{ minHeight: "100vh" }}>
@@ -109,154 +266,8 @@ class ContactPage extends React.Component {
             style={{ padding: "5rem 0" }}
             className="group container-l pad-h-container"
           >
-            <div className="c-2-3">
-              <h2>You can find me...</h2>{" "}
-              <ul>
-                <li>
-                  talking about software, distributed teams & work on{" "}
-                  <a href="https://twitter.com/chaseadamsio">Twitter</a>.
-                </li>{" "}
-                <li>
-                  building software on{" "}
-                  <a href="https://gitlab.com/chaseadamsio">GitLab</a> and{" "}
-                  <a href="https://github.com/chaseadamsio">GitHub</a>.
-                </li>
-                <li>
-                  also writing on{" "}
-                  <a href="https://medium.com/chaseadamsio">Medium</a>.
-                </li>
-                <li>
-                  professionally on{" "}
-                  <a href="https://www.linkedin.com/in/chaseadams/">LinkedIn</a>.
-                </li>
-              </ul>
-              <p style={{ fontStyle: "italic" }}>
-                Can't find a place that makes sense? Reach out through the form!
-              </p>
-            </div>
-
-            <div className="c-1-3">
-              {this.state.errors.server && <p>{this.state.errors.server}</p>}
-              {this.state.status === "success" && (
-                <p>Thanks for reaching out!</p>
-              )}
-              <form
-                name="contact"
-                method="post"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                <input type="hidden" name="form-name" value="contact" />
-                <p hidden>
-                  <label>
-                    Don’t fill this out: <input name="bot-field" />
-                  </label>
-                </p>
-
-                <div
-                  className={
-                    shouldMarkError("reason") ? "error input" : "input"
-                  }
-                >
-                  <select
-                    value={input.reason}
-                    onBlur={this.handleBlur("reason")}
-                    onChange={this.handleChange}
-                    name="reason"
-                  >
-                    <option value="" disabled="" defaultValue="">
-                      What's up?
-                    </option>
-                    {[
-                      "I want to learn something.",
-                      "I want to recruit you.",
-                      "Just saying hi."
-                    ].map((option, idx) => {
-                      return (
-                        <option key={idx} name="reason" value={option}>
-                          {option}
-                        </option>
-                      );
-                    })}
-                  </select>
-                  {shouldMarkError("reason") && (
-                    <span className="error">
-                      Choosing a reason helps me respond better!
-                    </span>
-                  )}
-                </div>
-
-                <div className="group pad-bottom-container">
-                  <div
-                    className={
-                      shouldMarkError("name") ? "error input" : "input"
-                    }
-                  >
-                    <label htmlFor="name">name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      autoComplete="name"
-                      placeholder="Chase Adams"
-                      value={input.name}
-                      onBlur={this.handleBlur("name")}
-                      onChange={this.handleChange}
-                    />
-                    {shouldMarkError("name") && (
-                      <span>Your name helps me know who you are!</span>
-                    )}
-                  </div>
-
-                  <div
-                    className={
-                      shouldMarkError("email") ? "error input" : "input"
-                    }
-                  >
-                    <label htmlFor="email">email</label>
-                    <input
-                      type="text"
-                      name="email"
-                      autoComplete="email"
-                      placeholder="hey@chaseadams.io"
-                      value={input.email}
-                      onBlur={this.handleBlur("email")}
-                      onChange={this.handleChange}
-                    />
-                    {shouldMarkError("email") && (
-                      <span>Knowing your email enables me respond!</span>
-                    )}
-                  </div>
-                </div>
-
-                <div
-                  className={
-                    shouldMarkError("message") ? "error input" : "input"
-                  }
-                >
-                  <label>message</label>
-                  <textarea
-                    name="message"
-                    placeholder="Hey, I've got something to say!"
-                    value={input.message}
-                    onBlur={this.handleBlur("message")}
-                    onChange={this.handleChange}
-                  />
-                  {shouldMarkError("message") && (
-                    <span>A message gives me context!</span>
-                  )}
-                </div>
-                <div>
-                  <button
-                    className={!isEnabled ? "error" : ""}
-                    disabled={!isEnabled}
-                    type="submit"
-                  >
-                    {!isEnabled ? "The form is incomplete..." : "Send"}
-                  </button>
-                </div>
-              </form>
-            </div>
+            <ContactInfo />
+            <ContactForm />
           </div>
         </article>
       </Layout>
