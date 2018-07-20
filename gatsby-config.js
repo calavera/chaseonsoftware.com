6755eb8d95d7f3df09e4231a1a554b77810ddb4b
Chase Adams <hey@chaseadams.io>
2018-07-19 22:08:18 -0700

    add autolink headers for remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ const cfg = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [`gatsby-remark-prismjs`]
+        plugins: [`gatsby-remark-prismjs`, `gatsby-remark-autolink-headers`]
       }
     },
     {
