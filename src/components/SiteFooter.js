d1b5cd752a7ec1096f20e4da10ab637c7e3a5bd7
Chase Adams <hey@chaseadams.io>
2018-08-20 07:21:05 -0700

    style: update footer and header colors

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -2,27 +2,31 @@ import React from "react";
 
 export default () => (
   <footer>
-    <p>
-      <a href="/">chaseadams.io</a> is powered by GatsbyJS, GitLab & Netlify.<br />
-    </p>
-    <p>
-      Read more about the topology in the <a href="/site-guide">Site Guide</a>.
-    </p>
-    <p>
-      Find the source for
-      <a href="https://gitlab.com/chaseadamsio/chaseadamsio.gitlab.io">
-        {" "}
-        chaseadams.io
-      </a>{" "}
-      on <a href="https://gitlab.com">Gitlab.</a>
-    </p>
-    <p>
-      <a href="/contact/">
-        <span role="img" aria-label="waving">
-          👋
-        </span>{" "}
-        Say Hi!
-      </a>
-    </p>
+    <div className="footer-inner">
+      <p>
+        <a href="/">chaseadams.io</a> is powered by GatsbyJS, GitLab & Netlify.
+        <br />
+      </p>
+      <p>
+        Read more about the topology in the <a href="/site-guide">Site Guide</a>
+        .
+      </p>
+      <p>
+        Find the source for
+        <a href="https://gitlab.com/chaseadamsio/chaseadamsio.gitlab.io">
+          {" "}
+          chaseadams.io
+        </a>{" "}
+        on <a href="https://gitlab.com">Gitlab.</a>
+      </p>
+      <p>
+        <a href="/contact/">
+          <span role="img" aria-label="waving">
+            👋
+          </span>{" "}
+          Say Hi!
+        </a>
+      </p>
+    </div>
   </footer>
 );
