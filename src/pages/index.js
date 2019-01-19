f3b5e7b23fec65b0f89235662dce933adc294d75
Chase Adams <hey@chaseadams.io>
2019-01-19 14:42:21 -0800

    add category to frontmatter graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,6 +67,7 @@ export const query = graphql`
             description
             date(formatString: "YYYY/MM/DD")
             tags
+            category
           }
           excerpt
         }
